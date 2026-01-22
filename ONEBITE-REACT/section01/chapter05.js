58696bfcd4b489e3a7743738a526c56ca5867be0
kim <rlaalswo2437@gmail.com>
2026-01-22 22:10:42 +0900

    "자료형"

---
+++ ONEBITE-REACT/section01/chapter05.js
@@ -0,0 +1,26 @@
+// 1. Number
+let num1 = -27;
+let num2 1.5;
+let num3 -20;
+
+
+let int = Infinity;
+let nav = Infinity;
+let nan = NaN;
+
+//2. String Type
+let myName = "이정환";
+let myLocation = "목동";
+let introduce = myName + myLocation;
+
+let introduceText = myName + myLocation;
+console.log(introduceText);
+
+// 3. Boolean Type
+//템플릿 리터럴 문법
+
+// 4.Null Type
+let empty = null;
+
+// 5. Undefined Type
+let none;

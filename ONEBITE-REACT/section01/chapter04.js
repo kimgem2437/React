92a248320ca8dea7d33a63d051424a1dce52a1a1
kim <rlaalswo2437@gmail.com>
2026-01-22 21:58:55 +0900

    "변수와 상수"

---
+++ ONEBITE-REACT/section01/chapter04.js
@@ -0,0 +1,22 @@
+// 1. 변수
+let age;
+
+age = 30;
+
+// 2. 상수
+const birth = "1997.01.07";
+
+// 3. 변수 명명규칙(네이밍 규칙)
+// 3-1. $, _제외한 기호는 사용할 수 없다.
+let $_name;
+
+// 3-2. 숫자로 시작할 수 없다.
+let name1;
+let $2name;
+
+// 3-3. 예약어를 사용할 수 없다.
+
+// 4. 변수 명명 가이드
+let saleCount = 1;
+let refundCount = 2;
+let totalScalesCount = saleCount - refundCount;

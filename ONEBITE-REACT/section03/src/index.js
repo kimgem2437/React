089919dda1a3259737e9f06511273143d3e1228e
kim <rlaalswo2437@gmail.com>
2026-02-11 23:55:08 +0900

    "Node.js 라이브러리 사용하기"

--- ONEBITE-REACT/section03/src/index.js
+++ ONEBITE-REACT/section03/src/index.js
@@ -1,5 +1,12 @@
 import mul, {add, sub} from './math.js';
+import randomColor from "randomcolor";
 
-console.log(add(1,2));
-console.log(sub(1,2));
-console.log(mul(2,3));
+const color = randomColor();
+console.log(color);
+
+
+
+
+// console.log(add(1,2));
+// console.log(sub(1,2));
+// console.log(mul(2,3));

b895cde1df99e21db805bbcca640138795df32d7
kim <rlaalswo2437@gmail.com>
2026-02-17 13:50:02 +0900

    "React 컴포넌트"

--- ONEBITE-REACT/section05/src/App.jsx
+++ ONEBITE-REACT/section05/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 
 function App() {
-
   return (
     <>
-        <h1>안녕 리액트!</h1>
+        <Header />
+        <Main />
+        <Footer />
     </>
   )
 }
